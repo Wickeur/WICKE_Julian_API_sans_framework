d01fe201eef333c1ce60800cf6f2a574e02c647e
Wickeur <wicke.julian@gamil.com>
2023-12-17 20:22:36 +0100

    bootstrap

--- Front/script.js
+++ Front/script.js
@@ -36,7 +36,7 @@ function displayClassList(classes) {
 
     // Création de l'en-tête du tableau
     var headerRow = document.createElement('tr');
-    headerRow.innerHTML = '<th>Nom de la classe</th><th>Niveau</th><th>Actions</th>';
+    headerRow.innerHTML = '<th scope="col">Nom de la classe</th><th scope="col">Niveau</th><th scope="col">Actions</th>';
     classTable.appendChild(headerRow);
 
     // Parcours des classes
@@ -47,8 +47,8 @@ function displayClassList(classes) {
             // Création d'une ligne pour chaque classe
             var row = document.createElement('tr');
             row.innerHTML = '<td>' + classItem.name + '</td>' +
-                            '<td>' + classItem.level + '</td>' +
-                            '<td><button class="btn btn-primary" onclick="redirectToUpdatePageClass(' + classId + ')">Modifier</button></td>';
+                '<td>' + classItem.level + '</td>' +
+                '<td><button class="btn btn-primary" onclick="redirectToUpdatePageClass(' + classId + ')">Modifier</button></td>';
 
             classTable.appendChild(row);
         }
@@ -62,15 +62,15 @@ function redirectToUpdatePageClass(classId) {
 
 // Fonction pour récupérer le nom de la classe à partir de l'ID de classe
 function getClassName(classId) {
-    return new Promise(function(resolve, reject) {
+    return new Promise(function (resolve, reject) {
         $.ajax({
             url: 'http://localhost:8000/class',
             type: 'GET',
-            success: function(response) {
+            success: function (response) {
                 var className = response[classId].name;
                 resolve(className);
             },
-            error: function(error) {
+            error: function (error) {
                 reject('Erreur lors de la récupération du nom de la classe:', error);
             }
         });
@@ -84,7 +84,7 @@ async function displayStudentList(students) {
 
     // Création de l'en-tête du tableau
     var headerRow = document.createElement('tr');
-    headerRow.innerHTML = '<th>Nom</th><th>Prénom</th><th>Email</th><th>Téléphone</th><th>Adresse</th><th>Code Postal</th><th>Ville</th><th>Classe</th><th>Actions</th>';
+    headerRow.innerHTML = '<th scope="col">Nom</th><th scope="col">Prénom</th><th scope="col">Email</th><th scope="col">Téléphone</th><th scope="col">Adresse</th><th scope="col">Code Postal</th><th scope="col">Ville</th><th scope="col">Classe</th><th scope="col">Actions</th>';
     studentTable.appendChild(headerRow);
 
     // Parcours des étudiants
@@ -98,14 +98,14 @@ async function displayStudentList(students) {
                 // Création d'une ligne pour chaque étudiant
                 var row = document.createElement('tr');
                 row.innerHTML = '<td>' + studentItem.lastname + '</td>' +
-                                '<td>' + studentItem.firstname + '</td>' +
-                                '<td>' + studentItem.email + '</td>' +
-                                '<td>' + studentItem.phone + '</td>' +
-                                '<td>' + studentItem.address + '</td>' +
-                                '<td>' + studentItem.zip + '</td>' +
-                                '<td>' + studentItem.city + '</td>' +
-                                '<td>' + className + '</td>' +
-                                '<td><button class="btn btn-primary" onclick="redirectToUpdatePageStudent(' + studentId + ')">Modifier</button></td>';
+                    '<td>' + studentItem.firstname + '</td>' +
+                    '<td>' + studentItem.email + '</td>' +
+                    '<td>' + studentItem.phone + '</td>' +
+                    '<td>' + studentItem.address + '</td>' +
+                    '<td>' + studentItem.zip + '</td>' +
+                    '<td>' + studentItem.city + '</td>' +
+                    '<td>' + className + '</td>' +
+                    '<td><button class="btn btn-primary" onclick="redirectToUpdatePageStudent(' + studentId + ')">Modifier</button></td>';
 
                 studentTable.appendChild(row);
             } catch (error) {
@@ -132,11 +132,11 @@ function addClass() {
         type: 'POST',
         contentType: 'application/json',
         data: JSON.stringify({ name: className, level: classLevel }),
-        success: function(response) {
+        success: function (response) {
             // Rechargez la page après l'ajout de la classe
             location.reload();
         },
-        error: function(error) {
+        error: function (error) {
             console.error('Erreur lors de l\'ajout de la classe:', error);
         }
     });
